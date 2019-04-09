c1e437f25480041bc6fc95aaf7964d026fc0e519
Matt Price <matt@mattprice.eu>
2019-04-09 20:55:06 +0100

    refactoring

--- Directorii/SplashForm.cs
+++ Directorii/SplashForm.cs
@@ -17,19 +17,29 @@ namespace Directorii
     public partial class SplashForm : Form
     {
 
+        #region private variables
         private Settings loadedSettings = null;
         private List<ADContainer> listOfAdContainers = new List<ADContainer>();
         private string savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "ADSyncTool");
         private DirectorySearcher search = null;
+        #endregion
+
+        #region getters and setters
+        internal List<ADContainer> ListOfAdContainers { get => listOfAdContainers; set => listOfAdContainers = value; }
+        public string SavePath { get => savePath; set => savePath = value; }
+        public Settings LoadedSettings { get => loadedSettings; set => loadedSettings = value; }
+        public DirectorySearcher Search { get => search; set => search = value; }
+        #endregion
+
 
         public SplashForm()
         {
             InitializeComponent();
-            Console.WriteLine("PATH: " + savePath);
             System.IO.Directory.CreateDirectory(savePath);
             loadSettingsJSON();
         }
 
+        #region settings JSON
         private void loadSettingsJSON()
         {
             try
@@ -58,7 +68,42 @@ namespace Directorii
                 Console.WriteLine("Could not load directory objects list. No directory objects JSON found?");
             }
         }
+        private void serializeSettingsToJSON()
+            {
+                if (LoadedSettings != null)
+                {
+                    try
+                    {
+                        LoadedSettings.ListOfADContainers = ListOfAdContainers;
+                        //writing Settings file
+                        string output = JsonConvert.SerializeObject(LoadedSettings, Formatting.Indented);
+                        StreamWriter sw = new StreamWriter(savePath + "\\settings.json");
+                        sw.Write(output);
+                        sw.Close();
 
+                        //writing Directory Objects file
+                        string directoryObjectsOutput = JsonConvert.SerializeObject(LoadedSettings.ListOfADContainers, Formatting.Indented);
+                        StreamWriter directoryWriter = new StreamWriter(savePath + "\\directoryList.json");
+                        directoryWriter.Write(directoryObjectsOutput);
+                        directoryWriter.Close();
+
+                    } catch (IOException)
+                    {
+                        MessageBox.Show("Unable to write settings file. Configuration has not been saved.");
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        MessageBox.Show("Permisson denied when writing settings file. Configuration has not been saved.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("SKIPPING WRITE");
+                }
+            }
+        #endregion
+
+        #region event handlers
         private void button1_Click(object sender, EventArgs e)
         {
             //Attempt to load from Settings object, if it exists. Create a blank one if not
@@ -84,55 +129,9 @@ namespace Directorii
             directoryForm.ShowDialog();
         }
 
-        private void serializeSettingsToJSON()
-        {
-            if (LoadedSettings != null)
-            {
-                try
-                {
-                    LoadedSettings.ListOfADContainers = ListOfAdContainers;
-                    //writing Settings file
-                    string output = JsonConvert.SerializeObject(LoadedSettings, Formatting.Indented);
-                    StreamWriter sw = new StreamWriter(savePath + "\\settings.json");
-                    sw.Write(output);
-                    sw.Close();
-
-                    //writing Directory Objects file
-                    string directoryObjectsOutput = JsonConvert.SerializeObject(LoadedSettings.ListOfADContainers, Formatting.Indented);
-                    StreamWriter directoryWriter = new StreamWriter(savePath + "\\directoryList.json");
-                    directoryWriter.Write(directoryObjectsOutput);
-                    directoryWriter.Close();
-
-                } catch (IOException)
-                {
-                    MessageBox.Show("Unable to write settings file. Configuration has not been saved.");
-                }
-                catch (UnauthorizedAccessException)
-                {
-                    MessageBox.Show("Permisson denied when writing settings file. Configuration has not been saved.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("SKIPPING WRITE");
-            }
-        }
-
         private void SplashForm_Load(object sender, EventArgs e)
         {
 
-        }
-
-
-        internal List<ADContainer> ListOfAdContainers { get => listOfAdContainers; set => listOfAdContainers = value; }
-        public string SavePath { get => savePath; set => savePath = value; }
-        public Settings LoadedSettings { get => loadedSettings; set => loadedSettings = value; }
-        public DirectorySearcher Search { get => search; set => search = value; }
-
-        public void setDirectoryConnection(DirectorySearcher search)
-        {
-            this.Search = search;
-            openServerSettingsDialogButton.ForeColor = System.Drawing.Color.FromArgb(128, 255, 128);
         }
 
         private void openSyncDialogButton_Click(object sender, EventArgs e)
@@ -151,6 +150,13 @@ namespace Directorii
         private void openAdvancedDialogButton_Click(object sender, EventArgs e)
         {
 
+        }
+        #endregion
+
+        public void setDirectoryConnection(DirectorySearcher search)
+        {
+            this.Search = search;
+            openServerSettingsDialogButton.ForeColor = System.Drawing.Color.FromArgb(128, 255, 128);
         }
 
     }
